dc66174994b1ab18e1f1b9d16ef2cd11cbe0dfc4
potato <t@t.ra>
2022-01-04 11:19:31 +0700

    wip

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.js-slick').slick({
+        prevArrow: $('.js-prev'),
+        nextArrow: $('.js-next'),
+        slidesToShow: 1,
+    });
+    console.log(1);
+});
